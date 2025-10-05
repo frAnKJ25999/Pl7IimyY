12d5d5cb8e00418a6b5e6b77a9daea5acf4f4b03
frAnKJ25999 <hzyitong66@gmail.com>
2025-10-05 19:46:30 +0100

    Configure JAVA KYC身份验证

---
+++ KycApplication_1006_0246_waz.java
@@ -0,0 +1,12 @@
+// 代码生成时间: 2025-10-06 02:46:29
+package com.example.kyc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KycApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KycApplication.class, args);
+    }
+}
